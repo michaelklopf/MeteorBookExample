015d82f439e5096373d050415dbb8103ff294d42
Michael Klopf <klopf.michael@gmail.com>
2014-11-02 22:41:50 +0100

    creating posts, securing paths

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,13 @@
 Template.postsList.helpers({
   posts: function() {
-    return Posts.find();
+    return Posts.find({}, {sort: {submitted: -1}});
   }
 });
+
+/*
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    console.log('There are ' + Posts.find().count() + ' posts');
+  });
+});
+*/
