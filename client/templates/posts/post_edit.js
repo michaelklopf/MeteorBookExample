eb6f97de3d9955e46305d29c446754cde285fa09
Michael Klopf <klopf.michael@gmail.com>
2014-11-04 23:22:05 +0100

    Moved update to server

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -5,10 +5,23 @@ Template.postEdit.events({
     var currentPostId = this._id;
 
     var postProperties = {
+      _id: currentPostId,
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     }
 
+    Meteor.call('postEdit', postProperties, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
+
+      Router.go('postsList');
+    });
+    /*
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
@@ -17,6 +30,7 @@ Template.postEdit.events({
         Router.go('postPage', {_id: currentPostId});
       }
     });
+    */
   },
 
   'click .delete': function(e) {
