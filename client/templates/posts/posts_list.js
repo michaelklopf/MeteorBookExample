3ba02e934754b9c354cbaa76bf772c87c5f29cb4
Michael Klopf <klopf.michael@gmail.com>
2014-11-14 22:05:06 +0100

    Adding pagination.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,10 +1,10 @@
+/*
 Template.postsList.helpers({
   posts: function() {
     return Posts.find({}, {sort: {submitted: -1}});
   }
 });
 
-/*
 Meteor.startup(function() {
   Tracker.autorun(function() {
     console.log('There are ' + Posts.find().count() + ' posts');
